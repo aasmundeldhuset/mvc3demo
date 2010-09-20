f817723cabd53a120c33a6eb8b72180cf73e15c9
Aasmund Eldhuset <aasmund.eldhuset@bekk.no>
2010-09-21 01:39:48 +0200

    Formatter class.

---
+++ Commons/Format.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Commons
+{
+    public static class Format
+    {
+        public const string CultureName = "en-US";
+        public const string GradeFormat = "0.00";
+
+        private static readonly CultureInfo Culture = new CultureInfo(CultureName, false);
+
+        public static string Grade(decimal gradeValue)
+        {
+            return gradeValue.ToString(GradeFormat, Culture);
+        }
+    }
+}
