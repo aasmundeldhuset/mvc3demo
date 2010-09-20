4eed26557448bb26967fcfdd6407cfe70d1cef74
Aasmund Eldhuset <aasmund.eldhuset@bekk.no>
2010-09-20 23:37:59 +0200

    Simple grading.

--- BusinessLogic/GradeLogic.cs
+++ BusinessLogic/GradeLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Domain;
 using Domain.Repository;
 
 namespace BusinessLogic
@@ -18,6 +19,10 @@ namespace BusinessLogic
 
         public void GradeArticle(int articleId, string currentUserName, int gradeValue)
         {
+            var article = _repo.Get<Article>(articleId);
+            var user = _repo.GetWhere<User>(u => u.UserName == currentUserName).Single();
+            new Grade {Article = article, User = user, GradeValue = gradeValue};
+            _repo.SaveChanges();
         }
     }
 }
