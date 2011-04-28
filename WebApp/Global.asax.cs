425f2f50b771ce162c2ba0c02d93e1a74a3ad7c0
Aasmund Eldhuset <aasmund.eldhuset@bekk.no>
2011-04-28 16:06:39 +0200

    Using MVC3's dependency injection support to wrap Ninject instead of using NinjectWebApplication.

--- WebApp/Global.asax.cs
+++ WebApp/Global.asax.cs
@@ -1,40 +1,40 @@
 ﻿using System;
 using System.Configuration;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using BusinessLogic;
 using Commons;
-using Domain;
 using Migrations;
-using Ninject;
-using Ninject.Web.Mvc;
 using WebApp.Attributes;
 using WebApp.Binders;
 using log4net.Config;
 
 namespace WebApp
 {
-    public class MvcApplication : NinjectHttpApplication
+    public class MvcApplication : HttpApplication
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
             routes.MapRoute(
               "SampleEdit",
               "Article/{id}/R",
               new { controller = "Article", action = "Edit", validateAntiForgeryToken = true }
-         );
+            );
+
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
-          
         }
 
-        protected override void OnApplicationStarted()
+        protected void Application_Start()
         {
             XmlConfigurator.Configure();
+
+            NinjectContainerSetup.SetUp();
 
             AreaRegistration.RegisterAllAreas();
 
@@ -45,11 +45,6 @@ namespace WebApp
             ModelBinders.Binders[typeof(DateTime)] = new DateTimeBinder();
 
             MigrateDatabase();
-        }
-
-        protected override IKernel CreateKernel()
-        {
-            return new StandardKernel(new RepoModule(), new LogicModule());
         }
 
         private static void MigrateDatabase()
