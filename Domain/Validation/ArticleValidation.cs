b19fa600ad68482d0122c60f298feb3da0a7d2cc
Aasmund Eldhuset <aasmund.eldhuset@bekk.no>
2011-04-28 17:41:22 +0200

    Fixing some display names.

--- Domain/Validation/ArticleValidation.cs
+++ Domain/Validation/ArticleValidation.cs
@@ -1,21 +1,21 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Validation
 {
     public class ArticleValidation
     {
-        [DisplayName("Tittel")]
         [WordCount(5)]
         public string Title { get; set; }
 
+        [DisplayName("Publish date")]
+        public DateTime PublishDate { get; set; }
 
-        [DisplayName("Test")]
+        [DisplayName("Email address")]
+        public string EmailAddress { get; set; }
+
         [Required, StringLength(100, MinimumLength = 10)]
         public string Summary { get; set; }
-
-        //[UIHint("EmailAddress")]
-        [DataType(DataType.EmailAddress)]
-        public string EmailAddress { get; set; }
     }
 }
